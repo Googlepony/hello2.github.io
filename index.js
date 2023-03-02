9ef655fecc819039f722b436a3a35f44e95ba80b
Googlepony <ponybitly@gmail.com>
2023-03-02 15:27:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const multer = require('multer');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 const upload = multer({ dest: 'uploads/' });
 
